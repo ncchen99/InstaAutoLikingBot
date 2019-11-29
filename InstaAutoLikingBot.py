7be3863e086addae317feeeda6d847ffef353b0e
ncchen99 <ncchen99@gmail.com>
2019-11-30 00:05:49 +0800

    Add files via upload

---
+++ InstaAutoLikingBot.py
@@ -0,0 +1,59 @@
+from selenium import webdriver  # selenium 的用法可參見 5-7 節
+from time import sleep
+# 匯入 time 模組的 sleep() 函式
+username='***REMOVED***'
+password='***REMOVED***'
+def signIn(driver):
+        driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input').send_keys(username) 
+        driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input').send_keys(password + "\n") 
+
+def startFlash(driver):
+        driver.find_element_by_xpath('/html/body/div[3]/div/div/div[3]/button[2]').click() #按下斥再縮
+        driver.implicitly_wait(2)
+        url = 'https://www.instagram.com/'+ username+ '/following/'
+        driver.get(url)
+        sleep(4)
+        driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a/span').click()
+        for index in range(int(driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a/span').get_attribute('textContent'))):
+                sleep(3)
+                driver.find_element_by_xpath('/html/body/div[3]/div/div[2]/ul/div/li['+str(index+1)+']/div/div[1]/div').click()#用圖片按follow的人
+                #/html/body/div[3]/div/div[2]/ul/div/li[1]/div/div[1]/div[2]/div[1]/a
+                driver.implicitly_wait(2)
+                try: 
+                        driver.find_element_by_xpath('//*[@id="react-root"]/section/div/div/section/header/div/div[1]/div/div/div/a').click() #如果跑出現時就在按進去
+                except:
+                    print("already in page")
+                driver.implicitly_wait(2)
+                #點開貼文，開始案愛心
+                #allPostsLen=len(driver.find_elements_by_css_selector("div[class='v1Nh3 kIKUG  _bz0w']"))
+                firstPost=driver.find_element_by_css_selector("div[class='v1Nh3 kIKUG  _bz0w']")
+                print(firstPost)
+                firstPost.click()
+                sleep(2)
+                while True:
+                        try:
+                                print (driver.find_element_by_css_selector("span[class='glyphsSpriteHeart__outline__24__grey_9 u-__7']").get_attribute('textContent'))
+                                driver.find_element_by_css_selector("span[class='glyphsSpriteHeart__outline__24__grey_9 u-__7']").get_attribute('textContent')
+                                driver.find_element_by_css_selector("span[class='fr66n']").click()
+                        except:
+                                print("already liked!")
+                                
+                        try:
+                                driver.find_element_by_css_selector("a[class='HBoOv coreSpriteRightPaginationArrow']").click()
+                        except:
+                                break
+                        sleep(2)
+                        
+                driver.get(url)
+                driver.implicitly_wait(2)
+                driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a').click()
+                
+def main():
+        url = 'https://www.instagram.com/accounts/login/?source=auth_switcher' 
+        driver=webdriver.Chrome()  # 建立瀏覽器物件
+        driver.get(url)            
+        driver.maximize_window()   # 將視窗最大化
+        signIn(driver) #登入
+        driver.implicitly_wait(2)
+        startFlash(driver)
+main()
