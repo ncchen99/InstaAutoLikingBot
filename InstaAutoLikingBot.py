cc42f6e7c812907d56742e6d939a121bb28fd048
ncchen99 <ncchen99@gmail.com>
2020-05-30 14:16:50 +0800

    0529

--- InstaAutoLikingBot.py
+++ InstaAutoLikingBot.py
@@ -1,22 +1,33 @@
 from selenium import webdriver  # selenium 的用法可參見 5-7 節
 from time import sleep
+from selenium.webdriver.common.keys import Keys
 # 匯入 time 模組的 sleep() 函式
 
 username='***REMOVED***' #在這裡輸入你的userID
 password='***REMOVED***' #在這裡輸入你的密碼
 
 def signIn(driver):
-        driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[2]/div/label/input').send_keys(username) 
-        driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[3]/div/label/input').send_keys(password + "\n") 
+        driver.find_element_by_name('username').send_keys(username) 
+        driver.find_element_by_name('password').send_keys(password + "\n") 
 
 def startFlash(driver):
-        driver.find_element_by_css_selector("button[class='aOOlW   HoLwm ']").click() #按下斥再說
+        driver.find_element_by_xpath("//*[contains(text(), 'Not Now')]").click() #按下斥再說
         sleep(1)
         driver.implicitly_wait(2)
         url = 'https://www.instagram.com/'+ username
         driver.get(url)
         sleep(2)
+        max = int(driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a/span').get_attribute('textContent'))
         driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a/span').click()
+        
+#        followersList = driver.find_element_by_css_selector('div[role=\'dialog\'] ul')
+#        numberOfFollowersInList = len(followersList.find_elements_by_css_selector('li'))
+#        followersList.click()
+#        actionChain = webdriver.ActionChains(driver)
+#        while (numberOfFollowersInList < max):
+#            actionChain.key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
+#            numberOfFollowersInList = len(followersList.find_elements_by_css_selector('li'))
+#            print(numberOfFollowersInList)
         for index in range(int(driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a/span').get_attribute('textContent'))):
                 sleep(1)
                 # /html/body/div[4]/div/div[2]/ul/div/li[1]/div/div[2]/div[1]/div/div/a
@@ -44,8 +55,6 @@ def startFlash(driver):
                 sleep(2)
                 while True:
                         try:
-                                print (driver.find_element_by_css_selector("span[class='glyphsSpriteHeart__outline__24__grey_9 u-__7']").get_attribute('textContent'))
-                                driver.find_element_by_css_selector("span[class='glyphsSpriteHeart__outline__24__grey_9 u-__7']").get_attribute('textContent')
                                 driver.find_element_by_css_selector("span[class='fr66n']").click()
                         except:
                                 print("already liked!")
@@ -64,10 +73,11 @@ def startFlash(driver):
                 
 def main():
         url = 'https://www.instagram.com/accounts/login/?source=auth_switcher' 
-        driver=webdriver.Chrome()  # 建立瀏覽器物件
+        driver=webdriver.Chrome("/home/ncc/桌面/InstaAutoLikingBot/chromedriver")  # 建立瀏覽器物件
         driver.get(url)            
         driver.maximize_window()   # 將視窗最大化
+        sleep(2)
         signIn(driver) #登入
-        sleep(4)
+        sleep(3)
         startFlash(driver)
 main()
